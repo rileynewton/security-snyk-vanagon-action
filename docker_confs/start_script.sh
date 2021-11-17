295a5da1f721d3bd544512706cb488d1c4274c1d
Jeremy Mill <jeremymill@gmail.com>
2021-11-17 08:26:05 -0500

    add github ssh keys

--- docker_confs/start_script.sh
+++ docker_confs/start_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+mkdir -p /root/.ssh && ssh-keyscan github.com >> ~/.ssh/known_hosts
 mkdir -p /data/nginx/cache
 mv /nginx_config /etc/nginx/sites-available/default
 B64KEY=$(echo -n "$INPUT_RPROXYUSER:$INPUT_RPROXYKEY" | base64 -w0)
